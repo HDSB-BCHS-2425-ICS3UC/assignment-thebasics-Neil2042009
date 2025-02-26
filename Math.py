6a224e5bcdbbe184b1d79bcb7dfff8b207a8ea3f
Neil2042009 <1enei@hdsb.ca>
2025-02-26 14:26:12 -0500

    task

---
+++ Math.py
@@ -0,0 +1,31 @@
+addition = 2+2
+print("The addition is 2+2 and the result is", addition)
+#Addition is to add numbers
+
+subtraction = 2-2
+print("The subtraction is 2-2 and the result is", subtraction)
+#Subtraction is to subtract numbers
+
+multiplication = 2*3
+print("The multiplication is 2*3 and the result is", multiplication)
+#Multiplication is to multiply numbers
+
+division = 6/3
+print("The division is 6/3 and the result is", division)
+#Division is to divide the numbers
+
+quot_float = 24/7
+print("The division is 24/7 and the result is",quot_float)
+#Quotient Float is to divide thenumbers and find the result with decimal
+
+rem_1 = 7%4
+print("The Modulu is 7%4 and the result is",rem_1)
+#Modulu is to five us the remainder of WHOLE number division
+
+import math
+square = math.sqrt(25)
+print("The Square Root is square root 25 and the result is",square)
+#Square root is to find a square root of numbers
+
+nth_power = 2**4
+print("The exponents is 2*2*2(2**4) and the result is",nth_power)
--- Work.py
+++ Work.py
@@ -1,5 +1,14 @@
-print("...-2,-1,0-1-2...")
+print("integer: ...-2,-1,0-1-2...")
 #This is the integer, all whole numbers.
-print("1.1,2.4,3.5")
+print("Floats: 1.1,2.4,3.5")
 #This is the floats, numbers with decimal points
+print("Double: 2.1265467567...")
+#This is the Double, mumbers with decimal and more accurate
+print("Char: A,B,C,a,b,c...")
+#This is the Char, Single characters
+print("Booleans: True or False")
+live = True
+death = False
+print(live)
+print(death)
 
