a09419e2646eb1d5f463c30cba415015785e5428
Neil2042009 <1enei@hdsb.ca>
2025-02-28 14:27:48 -0500

    task

--- 3D volume.py
+++ 3D volume.py
@@ -1,1 +1,49 @@
-print("Hello, dear user, welcome to the volume o of the three-dimensional shapes calculator")
+import math
+Cube = 0
+Sphere = 1
+Cone = 2
+Cylinder = 3
+choice = float(input())
+
+def cube1():
+    length = float (input("Enter the length of your cube "))
+    return length **3
+
+def sphere1():
+    sphere = float (input("Enter the radius of your sphere "))
+    return (3/4)*math.pi*sphere**3
+
+def cone1():
+    cone = float (input("Enter the radius of your cone "))
+    h = float (input("Enter the height of your cone "))
+    return (3/4)*math.pi*cone**2*h
+
+def cylinder1():
+    cylinder = float (input("Enter the radius of your cylinder "))
+    h = float (input("Enter the height of your cylinder "))
+    return math.pi*cylinder**2*h
+
+
+
+def main():
+    print("Hello, dear user, welcome to the volume of the three-dimensional shapes calculator.")
+    print("Selcet the 3D sahpe you want to calculate the volume.")
+    print("If you want to choose Cube press 0, Sphere press 1, Cone press 2, Cylinder press 3.")
+    
+    if choice == "0":
+        volume = cube1()
+        lenght = float (input("Enter the length of your cube "))
+
+    elif choice =='1':
+        volume = sphere1()
+        sphere = float (input("Enter the radius of your sphere "))
+
+    elif choice =='2':
+        volume = cone1()
+        cone = float (input("Enter the radius of your cone "))
+        h = float (input("Enter the height of your cone "))
+
+    elif choice =='3':
+        volume = cylinder1()
+        cylinder = float (input("Enter the radius of your cylinder "))
+        h = float (input("Enter the height of your cylinder "))
--- Work.py
+++ Work.py
@@ -1,14 +1,21 @@
 print("integer: ...-2,-1,0-1-2...")
 #This is the integer, all whole numbers.
+
 print("Floats: 1.1,2.4,3.5")
-#This is the floats, numbers with decimal points
+#This is the floats, numbers with decimal points.
+
 print("Double: 2.1265467567...")
-#This is the Double, mumbers with decimal and more accurate
+#This is the Double, mumbers with decimal and more accurate.
+
 print("Char: A,B,C,a,b,c...")
-#This is the Char, Single characters
+#This is the Char, Single characters.
+
 print("Booleans: True or False")
 live = True
 death = False
 print(live)
 print(death)
+#Thisi is Booleans, They are the foundation of computer science, such as YEs/No, 1/0..
 
+print("The letter combination you see is the string")
+#This is the string, it is combination of characters.
